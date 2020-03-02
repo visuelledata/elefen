93e5028a47050854fdd43f7a8b1b4d53fe5802a5
Christopher Peralta <chris99994@gmail.com>
2020-03-02 23:17:55 +0800

    Some sorting changes

--- elefen-misc-subsets.R
+++ elefen-misc-subsets.R
@@ -1,20 +1,35 @@
 library(tidyverse)
+library(tidytext)
 dict <- read_csv("elefen_dict.csv")
 freq <- read_csv("elefen_frequency_tatoeba.csv")
+affixes <- 
+  read_lines("dont-upload.txt") %>% 
+  as.tibble() %>% 
+  mutate(value = str_split(value, " ")) %>% 
+  unnest() %>% 
+  filter(str_detect(value, "([A-Za-z]-)|(-[A-Za-z])")) %>% 
+  mutate(value = str_remove_all(value, '”|“|:|\\(|\\)|\\.'))
+
 
 freq %>% 
   inner_join(dict, by = c("words" = "word")) %>% 
   arrange(desc(freq)) %>%  
   write_csv("elefen_dict_by_frequency.csv") 
 
+common_affixes <- affixes %>% 
+  inner_join(dict, by = c("value" = "word")) %>% 
+  pull(value)
 
 dict %>% 
   filter(str_detect(gramatica, "sufisa|prefisa"), 
          !is.na(defini)) %>% 
   rowwise %>% 
   mutate(english = !is.na(en), 
-         random = rdunif(1, 1, 10000)) %>%
+         random = rdunif(1, 1, 10000), 
+         common = word %in% common_affixes) %>%
   arrange(desc(random)) %>% 
+  arrange(desc(common)) %>% 
   arrange(desc(english)) %>% 
+  filter(word != en) %>% 
   select(-random, -english) %>% 
-  write_csv("elefen-affixes.csv")
+  write_csv("elefen-affixes.csv") %>% View()
