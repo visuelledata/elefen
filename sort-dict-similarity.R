8c56ef309a9e7d9ca4fedd974746a9c71ce3a9bf
Christopher Peralta <chris99994@gmail.com>
2020-03-19 20:46:44 +0800

    German fix

--- sort-dict-similarity.R
+++ sort-dict-similarity.R
@@ -29,23 +29,29 @@ dict[sample(nrow(dict)),] %>%
 #------- German
 sim_scores <- 
   dict %>% 
-  select(de, word) %>% 
-  distinct(de, word) %>% 
+  select(de, word, en) %>% 
+  #distinct(de, word) %>% 
   #filter(!str_detect(en, " "), !str_detect(word, " ")) %>% 
+  mutate(proper_noun = str_detect(en, "[A-Z]"), 
+         de = str_to_lower(de), 
+         word = str_to_lower(word)) %>% 
   mutate(sim = stringdist::stringdist(de, word)) %>% 
+  mutate(de = if_else(proper_noun, str_to_title(de), identity(de)), 
+         word = if_else(proper_noun, str_to_title(word), identity(word))) %>% 
   arrange(sim) %>% 
   select(word, sim) %>% 
   distinct()
 
 dict %>% 
-  inner_join(sim_scores) %>% 
+  left_join(sim_scores) %>% 
   arrange(sim) %>% 
   select(-word_id, -sim) %>%  
-  select(word, gramatica, defini, de, everything()) %>%
+  select(word, gramatica, defini, de, everything()) %>% 
+  filter(str_detect(de, "[A-Z]")) %>% 
   write_csv("dict_similarity_alphabetic_de.csv")
 
 dict[sample(nrow(dict)),] %>% 
-  inner_join(sim_scores) %>% 
+  left_join(sim_scores) %>% 
   arrange(sim) %>% 
   select(-word_id, -sim, everything()) %>% 
   write_csv("dict_similarity_rand_de.csv")
